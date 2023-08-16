94a1872459f03be05ef2a66b9f369d1a53f8f649
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-16 06:45:20 +0000

    DELETE:Home title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')  #仮引数なので，引数が無ければ，デフォルトで''
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url # ルーティングを追加することによって，static_pages_home_urlというRailsヘルパーが使えるようになる
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    # assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -33,6 +34,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 end
