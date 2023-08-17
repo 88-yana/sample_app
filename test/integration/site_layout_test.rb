f86cce6506851cc9806db53fa4ec4ab352f54f74
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-17 02:24:14 +0000

    ADD: sign up

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,5 +13,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get about_path
     assert_select "title", full_title("About")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
